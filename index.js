7a236ed7d283bf692b3351695ceaebf2e3f583d3
Janice Chan <janice.chan1288@gmail.com>
2022-01-25 09:57:33 -0500

    commit tree c37d0db30bb65aff3f28da462b324a7a07fbeaba

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solutions in this file
+
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+      }
+
+      
+const gifts = ["teddy bear", "drone", "doll"];     
+function wrapGifts(gifts) {
+  for (let i = 0; i <gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  debugger;
+  }
+  return gifts;
+}
+wrapGifts(gifts);
+
+const names = ["Lucas", "Phil", "Janice"];
+function writeCards(names, event) {
+  let message = [];
+  for (let i = 0; i < names.length; i++) {
+  message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+}
+return message;
+}
+//console.log(writeCards(["Lucas", "Phil", "Janice"], "surprise"));
+
+function countDown(num) {
+  while (num >= 0) {
+  console.log(num);
+  num--;
+}
+}
+console.log(countDown(10));
